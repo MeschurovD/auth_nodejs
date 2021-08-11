5946cb75f9e90f7278c17799504247626e5ee530
MeschurovDenis <meschurovd@gmail.com>
2021-08-11 21:52:24 +0500

    [server]: add server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import authRouter from './authRouter.js'
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use('/auth', authRouter)
+
+const start = async () => {
+  try {
+    await mongoose.connect(`mongodb+srv://meschurov:meschurov123@cluster0.zbjfd.mongodb.net/auth_roles?retryWrites=true&w=majority`)
+    app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
